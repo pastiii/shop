e7da73b3e97e5a95f70623767816b80366f26091
zhuzhu <593343693@qq.com>
2018-08-15 13:40:06 +0800

    '页面布局'

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,6 +6,16 @@
  * Time: 17:45
  */
 
+
+/**
+ * test demo
+ * @return string
+ */
 function test_helpers() {
     return "It's OK";
+}
+
+
+function route_class() {
+    return str_replace(".", "-", Route::currentRouteName());
 }
